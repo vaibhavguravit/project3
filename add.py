4689189c12650334f35c3c68f3b1edd8eba57fcb
vaibhavguravit <vaibhavguravit@gmail.com>
2022-09-03 11:50:23 +0530

    initial changes

---
+++ add.py
@@ -0,0 +1,20 @@
+def addition(a,b):
+    ans=a+b
+    return ans
+num1=int(input("enter no:"))
+num2=int(input("enter no:"))
+
+answer = addition(num1,num2)
+print(f"addition of {num1} and {num2} is {answer}")
+def vaibhav():
+    c= int(input("enter first number: "))
+    d= int(input("enter second number: "))
+    output=c-d
+    return output
+prachi=vaibhav()
+print(prachi)
+
+e= 40
+h= 5
+boss= addition(e,h)
+print(boss)
